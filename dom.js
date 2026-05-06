773c9baae8b31553122776f4d3abd92b1c551c7f
Munchunnce <vimalkumarak478@gmail.com>
2026-05-06 17:19:40 +0530

    DOM learn

--- dom.js
+++ dom.js
@@ -1,5 +1,9 @@
 //  DOM manipulation Document Object Model
 
 // 1. getElementById
-const heading = document.getElementById('heading');
+// const heading = document.getElementById('heading');
+// console.log(heading);
+
+// 2. getElementsByTagName
+const heading = document.getElementsByTagName('h1');
 console.log(heading);
