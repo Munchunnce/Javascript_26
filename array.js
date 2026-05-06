64e8831ffec56cdf6b75ccf6b24865020bc041e8
Munchunnce <vimalkumarak478@gmail.com>
2026-05-06 12:11:16 +0530

    array learn filter loop

--- array.js
+++ array.js
@@ -1,17 +1,17 @@
-const actors = [
-    {
-        name: 'Vimal',
-        payment: 400,
-    },
-    {
-        name: 'Shiwangi',
-        payment: 300,
-    },
-    {
-        name: 'Shikha',
-        payment: 500,
-    },
-];
+// const actors = [
+//     {
+//         name: 'Vimal',
+//         payment: 400,
+//     },
+//     {
+//         name: 'Shiwangi',
+//         payment: 300,
+//     },
+//     {
+//         name: 'Shikha',
+//         payment: 500,
+//     },
+// ];
 
 // for(let i = 0; i < actors.length; i++) {
 //     actors[i].payment = actors[i].payment - 50;
@@ -22,7 +22,34 @@ const actors = [
 // })
 
 // for of loop
-for( let actor of actors) {
-    actor.payment = actor.payment -20;
-}
-console.log(actors);
+// for( let actor of actors) {
+//     actor.payment = actor.payment -20;
+// }
+// console.log(actors);
+
+
+const students = [
+    {
+        name: 'Vimal',
+        marks: 80,
+    },
+    {
+        name: 'shikha',
+        marks: 40,
+    },
+    {
+        name: 'Shiwangi',
+        marks: 32,
+    },
+];
+
+const failed = students.filter((student) => {
+    if(student.marks < 40) {
+        return true;
+    }
+    else {
+        return false;
+    }
+});
+
+console.log(failed);
